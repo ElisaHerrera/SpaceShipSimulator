95fe69a6d757d2346c464a976304f04d68051540
ElisaHerrera <102582595+ElisaHerrera@users.noreply.github.com>
2022-04-21 07:38:25 +0100

    Feature:añadido funcion que carga

--- src/Cargo.java
+++ src/Cargo.java
@@ -5,6 +5,16 @@ public class Cargo extends SpaceShip{
     public Cargo(String nombre, String matriculaGalactica, int aceleracion, int capacidadMaxima) {
         super(nombre, matriculaGalactica, aceleracion);
         this.capacidadMaxima = capacidadMaxima;
+        this.carga=0;
+    }
+    public void load(){
+        if(carga>capacidadMaxima){
+            System.out.println("esta lleno");
+        }else{
+            carga++;
+        }
+
+
     }
 
 
