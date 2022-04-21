812596b933ac11fdef37e4c83bb3adfa536efe93
ElisaHerrera <102582595+ElisaHerrera@users.noreply.github.com>
2022-04-21 12:55:35 +0100

    Fix: arreglado problema con el menu de la nave normal

---
+++ out/production/untitled104/Readme.md
@@ -0,0 +1,7 @@
+# Proyecto nave espacial
+
+aqui definiremos los distintos tipos de naves que hay y las funciones que tiene cada una aqui un enlace de cada nave
+
+* [nave](../Docs/SpaceShip.md)
+
+* [cargo](../Docs/Cargo.md)
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,24 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int salida=1;
         int opcion=0;
-        salida=sc.nextInt();
+        System.out.println("elige un tipo de nave a crear");
+        System.out.println("    1-para crear una nave");
+        System.out.println("    2-para crear una cargo");
+        opcion=sc.nextInt();
         sc.nextLine();
+
         if (opcion==1){
             SpaceShip nave=new SpaceShip("gato","23234234D",2);
             while(salida!=0){
+                System.out.println("que quieres hacer con la nave");
+                System.out.println("    0 salir de la nave");
+                System.out.println("    1 acelerar");
+                System.out.println("    2 frenar");
+                System.out.println("    3 parada de emergencia");
+                System.out.println("    4 mostrar nave");
+
+                salida=sc.nextInt();
+                sc.nextLine();
                 switch (salida){
                     case 0:
                         System.out.println("saliendo");
@@ -18,11 +31,16 @@ public class Main {
                         nave.speedUp();
                         break;
                     case 2:
+                        System.out.println("e");
                         nave.brake();
                         break;
                     case 3:
+                        System.out.println("h");
                         nave.emergencyStop();
                         break;
+                    case 4:
+                        System.out.println(nave);
+                    break;
                 }
             }
         }
@@ -48,6 +66,9 @@ public class Main {
                     case 5:
                         carguera.unload();
                     break;
+                    case 6:
+                        System.out.println(carguera);
+                        break;
                 }
             }
         }
