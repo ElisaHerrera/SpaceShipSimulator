6596056fbdb3c6649e08e3400052f2b72e9ae776
ElisaHerrera <102582595+ElisaHerrera@users.noreply.github.com>
2022-04-21 08:15:57 +0100

    Feature:a añadido menu para una nave normal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int salida=1;
+        int opcion=0;
+        salida=sc.nextInt();
+        sc.nextLine();
+        if (opcion==1){
+            SpaceShip nave=new SpaceShip("gato","23234234D",2);
+            while(salida!=0){
+                switch (salida){
+                    case 0:
+                        System.out.println("saliendo");
+                        break;
+                    case 1:
+                        nave.speedUp();
+                        break;
+                    case 2:
+                        nave.brake();
+                        break;
+                    case 3:
+                        nave.emergencyStop();
+                        break;
+                }
+            }
+        }
+
     }
 
 
